c9084980a3123ebc3b6ad4af52b1f3a656ead342
lupita alvarez <alv.lu1305@gmail.com>
2021-08-24 09:41:49 -0700

    signup page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState }from 'react';
+import SignUp from './components/SignUp';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+    <div className="newaccount">
+      <img src="https://images.unsplash.com/photo-1531123414780-f74242c2b052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGxhZ29zJTJDJTIwbmlnZXJpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"></img>
+      <header>
+      <h2>Create a New Account <br></br>Sign up!</h2>
+      <SignUp/>
       </header>
+      <footer></footer>
+  
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function SignUp(props) {
+    const [signUpForm, setSignUForm] = useState({
+        username: '',
+        password: ''
+    })
+
+    function onChangeHandler(e) {
+        e.target.name === 'username' ? setSignUForm({...signUpForm, username: e.target.value}) : setSignUForm({...signUpForm, password: e.target.value})
+        console.log(e);
+    }
+    function onSubmit(e) {
+      e.preventDefault()
+    }
+
+    return (
+        <form>
+            <input 
+              placeholder='Username' 
+              name='username'
+              value={signUpForm.username} 
+              onChange={onChangeHandler}
+              type='text'  
+            />
+
+            <input 
+              placeholder='Password' 
+              name='password' 
+              value={signUpForm.password} 
+              onChange={onChangeHandler} 
+              type='password'
+            />
+        
+        
+    <a href="#">Submit</a>
+        
+        </form>
+
+    )
+}
+
+export default SignUp;
