cc18ba4713409da7048af5b751ed4800c9e286d2
lupita alvarez <alv.lu1305@gmail.com>
2021-08-24 13:17:58 -0700

    removed pic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState }from 'react';
+import React from 'react';
 import SignUp from './components/SignUp';
 
 
@@ -9,7 +9,7 @@ function App() {
   return (
     
     <div className="newaccount">
-      <img src="https://images.unsplash.com/photo-1531123414780-f74242c2b052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGxhZ29zJTJDJTIwbmlnZXJpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" alt="woman"></img>
+      
       <header>
       <h2>Create a New Account <br></br>Sign up!</h2>
       <SignUp/>
